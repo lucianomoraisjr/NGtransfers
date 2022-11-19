1b0a44d913eac4f7227b0c951946327dc8d2493a
unknown <lucianomoraisjr@gmail.com>
2022-11-19 16:07:21 -0300

    fix: user registred

--- src/infra/repos/postgres/check-by-username.ts
+++ src/infra/repos/postgres/check-by-username.ts
@@ -5,9 +5,8 @@ import { CheckAccountByUserNameRepository } from '@/data/protocols/'
 export class PgCheckByUsername extends PgRepository implements CheckAccountByUserNameRepository {
   async checkByUsername (username: string): Promise<boolean> {
     const pgUserRepo = this.getRepository(PgUser)
-
-    await pgUserRepo.findOne({ username })
-
+    const verf = await pgUserRepo.findOne({ username })
+    if (verf != null || verf !== undefined) return true
     return false
   }
 }
